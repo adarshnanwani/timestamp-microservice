e17aef4c06d34a345933fb41ccf36ebcff58c4ed
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-02-18 12:29:27 +0530

    Add basic route to api/timestamp via express router

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const timestampApi = require("./routes/api/Timestamp");
 
 const app = express();
 
@@ -8,6 +9,8 @@ app.use(
     optionsSuccessStatus: 200
   })
 );
+
+app.use("/api/timestamp", timestampApi);
 
 const PORT = process.env.PORT || 5000;
 
