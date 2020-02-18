d6addd698799e96431bccae69226bce26b07c672
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-02-18 12:51:01 +0530

    Add timestamp api endpoints.

--- routes/api/Timestamp.js
+++ routes/api/Timestamp.js
@@ -3,7 +3,23 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json({ message: "This works!" });
+  const date_string = req.params.date_string;
+  const newDate = new Date();
+  res.json({ unix: newDate.getTime(), utc: newDate.toUTCString() });
+});
+
+const checkAndReturnDate = date_str => {
+  if (date_str.includes("-")) {
+    return new Date(date_str);
+  } else {
+    return new Date(parseInt(date_str));
+  }
+};
+
+router.get("/:date_string", (req, res) => {
+  const date_string = req.params.date_string;
+  const newDate = checkAndReturnDate(date_string);
+  res.json({ unix: newDate.getTime(), utc: newDate.toUTCString() });
 });
 
 module.exports = router;
