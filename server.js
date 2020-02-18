fb6d60c9b2f60003922234add1ad270843e11de1
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-02-18 12:25:10 +0530

    Add cors middleware to serverfile.

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+
+app.use(
+  cors({
+    optionsSuccessStatus: 200
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 
